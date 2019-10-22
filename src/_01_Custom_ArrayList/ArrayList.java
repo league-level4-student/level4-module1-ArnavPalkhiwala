d4854fb07fb71502b8ceb0330ce4e77b4c237c5d
League Student <league@iMac-23.attlocal.net>
2019-10-21 18:23:42 -0700

    ArrayList(ip) :100:

--- src/_01_Custom_ArrayList/ArrayList.java
+++ src/_01_Custom_ArrayList/ArrayList.java
@@ -1,35 +1,67 @@
 package _01_Custom_ArrayList;
 
+import java.util.Arrays;
+
 @SuppressWarnings("unchecked")
 
-public class ArrayList <T>{
-	
+public class ArrayList<T> {
+	T[] templates;
+
 	public ArrayList() {
+		templates = (T[]) new Object[0];
 	}
-	
+
 	public T get(int loc) throws IndexOutOfBoundsException {
-		
-		return null;
+
+		return templates[loc];
 	}
-	
+
 	public void add(T val) {
-		
+		T[] templatesTwo = (T[]) new Object[templates.length + 1];
+		for (int i = 0; i <= templates.length - 1; i++) {
+
+			templatesTwo[i] = templates[i];
+		}
+
+		templatesTwo[templates.length] = val;
+
+		templates = templatesTwo;
+		System.out.println(Arrays.toString(templates));
 	}
-	
+
 	public void insert(int loc, T val) throws IndexOutOfBoundsException {
-		
+
 	}
-	
+
 	public void set(int loc, T val) throws IndexOutOfBoundsException {
-		
+
 	}
-	
+
 	public void remove(int loc) throws IndexOutOfBoundsException {
-		
+		T[] templatesTwo = (T[]) new Object[templates.length - 1];
+		for (int i = 0; i < templates.length - 1; i++) {
+
+			if (i < loc) {
+				templatesTwo[i] = templates[i];
+
+			}
+			
+			else if (i == loc) {
+				
+			}
+			
+			else {
+			
+				templatesTwo[i - 1] = templates[i];
+			}
+		}
+
+		templates = templatesTwo;
+
 	}
-	
+
 	public boolean contains(T val) {
-		
+
 		return false;
 	}
 }
